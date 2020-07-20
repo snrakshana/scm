3bb1c28b4a0beb1fc2a634f355959c81aeac032c
snrakshana <nagarakshana.sridharan@gmail.com>
2020-07-20 09:20:57 +0530

    first try

--- SCM/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Customer, SalesPerson
-
-admin.site.register(Customer)
-admin.site.register(SalesPerson)
--- SCM/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts import views
-
-urlpatterns = [
-    path('sales_master/', views.salesMaster, name='sales_master'),
-    path('create_customer/', views.createCustomer, name='create_customer'),
-]
